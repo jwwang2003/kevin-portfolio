7d9c95262cf3e286ddc69014e2b9809f9fd07a97
Jun Wei Wang <jwwang.2003@gmail.com>
2021-12-15 04:53:39 -0800

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,22 @@
+import Skeleton from '../components/MainSkeleton'
+
+export default function Home() {
+  return (
+    <Skeleton home>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+    </Skeleton>
+  )
+}
---
+++ pages/portfolio.js
@@ -0,0 +1,11 @@
+import Skeleton from '../components/MainSkeleton'
+
+export default function Portfolio() {
+  return (
+    <>
+      <Skeleton>
+        <h1>This is the portfolio</h1>
+      </Skeleton>
+    </>
+  )
+}
