79a6449dd8b34392f7f5a01546178d5baeec7a48
Jimmy Wang <jwwang.2003@gmail.com>
2021-12-23 22:21:32 -0800

    ?

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 import { AiOutlineWeibo, AiFillWechat, AiOutlineMail } from 'react-icons/ai'
 
 const Footer: NextPage = () => {
-  return (<footer className="flex flex-row items-center w-full min-h-30 p-4 bg-gray-200 md:fixed md:bottom-0">
+  return (<footer className="flex flex-row items-center w-full min-h-30 p-4 bg-gray-100 md:fixed md:bottom-0">
       <span className="mr-auto font-thin text-sm">© KW Photography</span>
       <span className="flex flex-row text-2xl">
         <AiOutlineWeibo className="mr-2" />
