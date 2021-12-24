a6023317437631017472f0d21e8dde82d5c291f7
Jimmy Wang <jwwang.2003@gmail.com>
2021-12-24 06:42:29 -0800

    ahhhh.

--- utils/react-photo-gallery.umd.js
+++ utils/react-photo-gallery.umd.js
@@ -227,7 +227,7 @@
     var colCurrTopPositions = [];
 
     for (var i = 0; i < columns; i++) {
-      colLeftPositions[i] = round(i * (colWidth + margin * 2), 1);
+      colLeftPositions[i] = Math.floor(i * (colWidth + margin * 2), 1);
       colCurrTopPositions[i] = 0;
     } // map through each photo, then reduce thru each "column"
     // find column with the smallest height and assign to photo's 'top'
@@ -239,8 +239,8 @@
         acc = item < colCurrTopPositions[acc] ? i : acc;
         return acc;
       }, 0);
-      photo.top = colCurrTopPositions[smallestCol];
-      photo.left = colLeftPositions[smallestCol];
+      photo.top = Math.floor(colCurrTopPositions[smallestCol]);
+      photo.left = Math.floor(colLeftPositions[smallestCol]) - 1;
       colCurrTopPositions[smallestCol] = Math.floor(colCurrTopPositions[smallestCol] + photo.height + margin * 2); // store the tallest col to use for gallery height because of abs positioned elements
 
       var tallestCol = colCurrTopPositions.reduce(function (acc, item, i) {
