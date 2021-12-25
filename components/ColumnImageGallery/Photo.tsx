9d012e3ef8e8779ddd4df88549c5ff35df9f0299
Jimmy Wang <jwwang.2003@gmail.com>
2021-12-25 02:50:34 -0800

    stuff

--- components/ColumnImageGallery/Photo.tsx
+++ components/ColumnImageGallery/Photo.tsx
@@ -1,15 +1,28 @@
 // customize the photo element here
 // TODO: hovering and subtitle with redirect
+import Link from 'next/link'
 import { Thumbnail } from './types'
 
+import styles from './Photo.module.scss'
+
+const handleDragStart = (e: any) => e.preventDefault()
+
 const Photo = ({ index, src, top, left, height, width } : Thumbnail): JSX.Element => {
-  return <div key={`${src}-${index}`} style={{
-    position: 'absolute',
+  return <Link href={src} passHref><div key={`${src}-${index}`} className={styles.container} style={{
     top: top,
-    left: left
+    left: left,
+    height,
+    width
   }}>
-      <img src={src} alt="" height={height} width={width} />
+      <div className={`${styles.cover}`} style={{ height, width }}>
+        <span className="text-center">
+          <h1 className="text-4xl">Subtitle</h1>
+          <h3 className="font-light">— View —</h3>
+        </span>
+      </div>
+      <img onDragStart={handleDragStart} className={`${styles.image}`} src={src} alt="" height={height} width={width} />
     </div>
+  </Link>
 }
 
 export default Photo
